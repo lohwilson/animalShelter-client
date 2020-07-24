0c700e3ba24dc8edb79372910a9e3c000d468fcb
wilfredloh <lohwilfred19@gmail.com>
2020-07-25 02:32:48 +0800

    Updates

---
+++ precache-manifest.a55bf6edc87cc2774de447c85a00d138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b70f89cc6ea6b185870508569407c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ce9985c4b17bd88391",
+    "url": "/static/css/main.fa019e86.chunk.css"
+  },
+  {
+    "revision": "51cf614090998d4177db",
+    "url": "/static/js/2.6bcf55ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bcf55ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ce9985c4b17bd88391",
+    "url": "/static/js/main.e423dd72.chunk.js"
+  },
+  {
+    "revision": "981b32331d732e9b8a45",
+    "url": "/static/js/runtime-main.ccfb647d.js"
+  }
+]);
